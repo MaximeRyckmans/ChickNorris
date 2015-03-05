31bb17378ee8253fd351460acff1bb87b9f19d74
Pulinx Davy <davypulinx@hotmail.com>
2015-03-05 15:06:04 +0100

    testing own javascript calendar. Issue with going to previous month.

--- ChickNorris/WebContent/resources/scripts/party-form.js
+++ ChickNorris/WebContent/resources/scripts/party-form.js
@@ -1,13 +1,36 @@
 /**
  * 
  */
+var d = new Date();
+var currentMonth = d.getMonth();
+var currentYear = d.getFullYear();
 
-$('#calendar').datepicker({
-	inline: true,
-	firstDay: 1,
-	showOtherMonths: true,
-	dayNamesMin: ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za']
+$(function(){
+	var monthNames = ["Jan.", "Feb.", "Mrt.", "Apr.", "Mei", "Jun.",
+	                  "Jul.", "Aug.", "Sept.", "Okt.", "Nov.", "Dec."
+	                ];
+	
+	$("#currentMonth").text(monthNames[currentMonth]);
+	$("#currentYear").text(currentYear);
+	$("#Next").click(function() {
+		if (currentMonth < 11) {
+			currentMonth++;
+		} else if (currentMonth === 11){
+			currentMonth = 0;
+			currentYear++;
+		}
+		$("#currentMonth").text(monthNames[currentMonth]);
+		$("#currentYear").text(currentYear);
+	});
+
+	$("#Previous").click(function() {
+		if (currentMonth > 0) {
+			currentMonth - 1;
+		} else if (currentMonth === 0) {
+			currentMonth = 11;
+			currentYear - 1;
+		}
+		$("#currentMonth").text(monthNames[currentMonth]);
+		$("#currentYear").text(currentYear);
+	});
 });
-
-$('#form_date').datepicker();
-$('#form_time').timepicker();
