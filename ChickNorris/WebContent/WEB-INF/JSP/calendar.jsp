1b78c69e5692e3299f8fa0c5de426fc9e02e0b34
Pulinx Davy <davypulinx@hotmail.com>
2015-03-07 16:57:15 +0100

    Finished up the javascript main code to get a working calendar

--- ChickNorris/WebContent/WEB-INF/JSP/calendar.jsp
+++ ChickNorris/WebContent/WEB-INF/JSP/calendar.jsp
@@ -10,23 +10,42 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Calendar</title>
 <link rel="stylesheet" type="text/css" href="resources/css/bootstrap/bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="resources/css/party-form.css"/>
 </head>
 <body>
-	<div id="calendar">
+	<div id="calendarWrap">
 		<div id="calendarTop">
 			<div id="calendarTitle">
 				<a id="Previous" href="#"><span class="glyphicon glyphicon-chevron-left"></span></a>
-				<span id="currentMonth"></span>
-				<span class="glyphicon glyphicon-minus"></span>
-				<span id="currentYear"></span>
+				<div id="titleCalendar">
+					<span id="currentMonth"></span>
+					<span class="glyphicon glyphicon-minus"></span>
+					<span id="currentYear"></span>
+				</div>
 				<a id="Next" href="#"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+		</div>
+		<div id="calendarBody">
+			<div id="calendarDaysName">
+				<ul>
+					<li>ma.</li>
+					<li>di.</li>
+					<li>wo.</li>
+					<li>do.</li>
+					<li>vr.</li>
+					<li>za.</li>
+					<li id="sunday">zo.</li>
+				</ul>
+			</div>
+			<div id="calendarDays">
+				
 			</div>
 		</div>
 	</div>
 	<!-- Placed at the end of the document so the pages load faster -->
 	<script type="text/javascript" src="resources/scripts/jquery.js"></script>	
 	<script type="text/javascript" src="resources/scripts/bootstrap/bootstrap.min.js"></script>
-	<script type="text/javascript" src="resources/scripts/party-form.js"></script>
+	<script type="text/javascript" src="resources/scripts/party-form.js"></script>	
 	<script type="text/javascript" src="resources/scripts/bootstrap/docs.min.js"></script>
 	<script type="text/javascript" src="resources/scripts/bootstrap/ie10-viewport-bug-workaround.js"></script>
 </body>
--- ChickNorris/WebContent/resources/scripts/party-form.js
+++ ChickNorris/WebContent/resources/scripts/party-form.js
@@ -4,11 +4,17 @@
 var d = new Date();
 var currentMonth = d.getMonth();
 var currentYear = d.getFullYear();
+var monthNames = ["Jan.", "Feb.", "Mrt.", "Apr.", "Mei", "Jun.","Jul.", "Aug.", "Sept.", "Okt.", "Nov.", "Dec."];
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 $(function(){
-	var monthNames = ["Jan.", "Feb.", "Mrt.", "Apr.", "Mei", "Jun.",
-	                  "Jul.", "Aug.", "Sept.", "Okt.", "Nov.", "Dec."
-	                ];
+	
+	Date.prototype.getMonthName = function() {
+        return months[ this.getMonth() ];
+    };
+    Date.prototype.getDayName = function() {
+        return days[ this.getDay() ];
+    };
 	
 	$("#currentMonth").text(monthNames[currentMonth]);
 	$("#currentYear").text(currentYear);
@@ -21,16 +27,74 @@ $(function(){
 		}
 		$("#currentMonth").text(monthNames[currentMonth]);
 		$("#currentYear").text(currentYear);
+		fillDays();
 	});
 
 	$("#Previous").click(function() {
 		if (currentMonth > 0) {
-			currentMonth - 1;
+			var month = currentMonth - 1;
+			var year = currentYear;
+			d.setFullYear(year, month, 1)
+			currentMonth = d.getMonth();
 		} else if (currentMonth === 0) {
-			currentMonth = 11;
-			currentYear - 1;
+			var month = 11;
+			var year = currentYear - 1;
+			d.setFullYear(year, month, 1)
+			currentMonth = d.getMonth();
+			currentYear = d.getFullYear();
 		}
 		$("#currentMonth").text(monthNames[currentMonth]);
 		$("#currentYear").text(currentYear);
+		fillDays();
 	});
+	
+	fillDays();
 });
+
+function fillDays() {
+	var date = new Date();
+	var month = currentMonth + 1;
+	date.setFullYear(currentYear, month, 0);
+	
+	var daysInMonth = date.getDate();
+	
+	date.setFullYear(currentYear, currentMonth, 1);
+	
+	var day = date.getDayName();
+	
+	var html;
+	var weekStart = '<div id="week"><ul>';
+	var weekEnd = '</ul></div>';
+	var grayedOut = '<li></li>';
+	
+	for (var i = 1; i <= daysInMonth; i++) {
+		if(i === 1 && day === days[1]){
+			html = weekStart + '<li><span>1</span></li>'
+		} else if(i === 1 && day === days[2]){
+			html = weekStart + grayedOut + '<li><span>1</span></li>';
+		}else if(i === 1 && day === days[3]){
+			html = weekStart + grayedOut + grayedOut + '<li><span>1</span></li>';
+		} else if(i === 1 && day === days[4]){
+			html = weekStart + grayedOut + grayedOut + grayedOut +'<li><span>1</span></li>';
+		} else if(i === 1 && day === days[5]){
+			html = weekStart + grayedOut + grayedOut + grayedOut + grayedOut + '<li><span>1</span></li>';
+		} else if(i === 1 && day === days[6]){
+			html = weekStart + grayedOut + grayedOut + grayedOut + grayedOut + grayedOut + '<li><span>1</span></li>';
+		} else if(i === 1 && day === days[0]){
+			html = weekStart + grayedOut + grayedOut + grayedOut + grayedOut + grayedOut + grayedOut +'<li><span>1</span></li>' + weekEnd;
+		} else if (day === days[1]){
+			html += weekStart + '<li><span>' + i + '</span></li>';
+		} else if (day === days[0]){
+			html += '<li><span>' + i + '</span></li>' + weekEnd;
+		} else {
+			html += '<li><span>' + i + '</span></li>';
+		}	
+		
+		date.setFullYear(currentYear, currentMonth, i + 1);
+		day = date.getDayName();
+	}
+	
+	html += weekEnd;
+	
+	document.getElementById("calendarDays").innerHTML = html;
+}
