310a749ad0527e456a5f9d783fa4bec2edcd64d9
Maxime Ryckmans <maximeryckmans@gmail.com>
2015-03-09 20:25:13 +0100

    Added some more backend in preparation of the admin page frontend. In AdminServlet there are errors because I need to get parameters from admin page

---
+++ ChickNorris/src/be/chickNorris/dao/CalendarDAO.java
@@ -0,0 +1,39 @@
+package be.chickNorris.dao;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import be.chickNorris.models.Calendar;
+import be.chickNorris.models.Customer;
+
+public class CalendarDAO extends AbstractDAO {
+
+	public void create(Calendar calendar) {
+		getEntityManager().persist(calendar);
+	}
+
+	public Calendar read(int ID) {
+		return getEntityManager().find(Calendar.class, ID);
+	}
+
+	public void delete(int ID) {
+		Customer customer = getEntityManager().find(Customer.class, ID);
+		if (customer != null) {
+			getEntityManager().remove(customer);
+		}
+	}
+
+	/**
+	 * Returns a list of Customer objects.
+	 *
+	 * @param projectCode
+	 * @return
+	 */
+	public List<Calendar> getAllCalendars() {
+		TypedQuery<Calendar> query = getEntityManager().createNamedQuery("selectAllCalenders", Calendar.class);
+
+		return query.getResultList();
+	}
+
+}
